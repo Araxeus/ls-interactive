8e4df1465a473addf999c3d7936a038afbe4ac0c
Araxeus <78568641+Araxeus@users.noreply.github.com>
2022-05-14 01:40:07 +0300

    use curl to version check

--- src/check_version.rs
+++ src/check_version.rs
@@ -1,14 +1,16 @@
 use console::style;
-use update_informer::{registry, Check};
 
 pub fn run() {
+    std::thread::spawn(|| {
+        check_version();
+    });
+}
+
+pub fn check_version() {
     let pkg_name = env!("CARGO_PKG_NAME");
     let current_version = env!("CARGO_PKG_VERSION");
 
-    let informer =
-        update_informer::new(registry::GitHub, "Araxeus/ls-interactive", current_version);
-
-    if let Ok(Some(version)) = informer.check_version() {
+    if let Some(version) = get_latest_version() {
         let msg = format!(
             "A new release of {pkg_name} is available: v{current_version} -> {new_version}",
             pkg_name = style(pkg_name).italic().cyan(),
@@ -28,4 +30,20 @@ pub fn run() {
             url = style(release_url).yellow()
         );
     }
+}
+
+
+pub fn get_latest_version() -> Option<String> {
+        let output = std::process::Command::new("curl")
+        .arg("--silent")
+        .arg("https://api.github.com/repos/Araxeus/ls-interactive/releases/latest")
+        .output();
+
+        match output {
+            Ok(output) => {
+                let stdout = String::from_utf8_lossy(&output.stdout);
+                Some(stdout.split("\"tag_name\": \"").nth(1).unwrap().split('\"').next().unwrap().trim_start_matches('v').to_string())
+            },
+            Err(_) => None,
+        }
 }
