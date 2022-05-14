f72ad1cb2cd3f008b87873b843c66b9ebf088cbf
Araxeus <78568641+Araxeus@users.noreply.github.com>
2022-05-14 17:31:24 +0300

    use notify-rust for update notification

--- src/check_version.rs
+++ src/check_version.rs
@@ -1,4 +1,4 @@
-use console::style;
+use notify_rust::Notification;
 
 pub fn run() {
     std::thread::spawn(|| {
@@ -10,40 +10,47 @@ pub fn check_version() {
     let pkg_name = env!("CARGO_PKG_NAME");
     let current_version = env!("CARGO_PKG_VERSION");
 
-    if let Some(version) = get_latest_version() {
-        let msg = format!(
-            "A new release of {pkg_name} is available: v{current_version} -> {new_version}",
-            pkg_name = style(pkg_name).italic().cyan(),
-            current_version = current_version,
-            new_version = style(version.to_string()).green()
-        );
-
-        let release_url = format!(
-            "https://github.com/{pkg_name}/{pkg_name}/releases/tag/{version}",
-            pkg_name = pkg_name,
-            version = version
-        );
-
-        println!(
-            "\n{msg}\n{url}",
-            msg = msg,
-            url = style(release_url).yellow()
-        );
+    if let Some(new_version) = get_latest_version() {
+        if new_version != current_version {
+            Notification::new()
+                .summary(pkg_name)
+                .body(&format!(
+                    "A new release of {pkg_name} is available: \n\
+    v{current_version} -> v{new_version}\n\
+    https://github.com/Araxeus/{pkg_name}/releases/tag/{new_version}",
+                    pkg_name = pkg_name,
+                    current_version = current_version,
+                    new_version = new_version
+                ))
+                .icon("/usr/share/icons/hicolor/256x256/apps/gnome-software.png")
+                .timeout(5000)
+                .show()
+                .ok();
+        }
     }
 }
 
-
 pub fn get_latest_version() -> Option<String> {
-        let output = std::process::Command::new("curl")
+    let output = std::process::Command::new("curl")
         .arg("--silent")
         .arg("https://api.github.com/repos/Araxeus/ls-interactive/releases/latest")
         .output();
 
-        match output {
-            Ok(output) => {
-                let stdout = String::from_utf8_lossy(&output.stdout);
-                Some(stdout.split("\"tag_name\": \"").nth(1).unwrap().split('\"').next().unwrap().trim_start_matches('v').to_string())
-            },
-            Err(_) => None,
+    match output {
+        Ok(output) => {
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            Some(
+                stdout
+                    .split("\"tag_name\": \"")
+                    .nth(1)
+                    .unwrap()
+                    .split('\"')
+                    .next()
+                    .unwrap()
+                    .trim_start_matches('v')
+                    .to_string(),
+            )
         }
+        Err(_) => None,
+    }
 }
